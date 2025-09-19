4cc282610d97a37e7b642d4e3e188394cdb9cb62
Fernando <fernando.huerta984@gmail.com>
2025-09-19 08:14:25 -0300

    entidades corregidas y completas

--- src/orders/entities/orders/orders.ts
+++ src/orders/entities/orders/orders.ts
@@ -1,1 +1,22 @@
-export class Orders {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './../../../users/entities/user/user';
+import { Driver } from './../../../drivers/entities/drivers/drivers';
+
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  status: string;
+
+  @Column('timestamp')
+  createdAt: Date;
+
+  @ManyToOne(() => User, user => user.id)
+  user: User;
+
+  @ManyToOne(() => Driver, driver => driver.id, { nullable: true })
+  driver: Driver;
+}
