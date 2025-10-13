15f8a3ba1fcb3a476ef13a10b30bb99bd3468754
Juan <nachobosque09@gmail.com>
2025-10-12 22:21:24 -0300

    CRUD de orders (lineas de drivers comentadas)

--- src/orders/entities/dto/create-orders.dto.ts
+++ src/orders/entities/dto/create-orders.dto.ts
@@ -1,9 +1,9 @@
 import { User } from "src/users/entities/user/user";
-import { Driver } from "src/drivers/entities/drivers/drivers";
-
+/* import { Driver } from "src/drivers/entities/drivers/drivers";
+ */
 export class CreateOrdersDto {
     readonly User: User;
     readonly createdAt: Date;    
-    readonly driver: Driver;
-    readonly status?: string;
+/*     readonly driver: Driver;
+ */    readonly status?: string;
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceInterface } from 'src/shared/interfaces/service.interface';
+import { Order } from './entities/orders/orders';
+import { Repository } from 'typeorm';
+import { CreateOrdersDto } from './entities/dto/create-orders.dto';
+/* import { User } from 'src/users/entities/user/user';
+ */
 @Injectable()
-export class OrdersService {}
+export class OrdersService implements ServiceInterface {
+    constructor(
+        @InjectRepository(Order)
+        private readonly orderRepository: Repository<Order>
+    ) {}
+
+    findAll(): Promise<Order[]> {
+        return this.orderRepository.find({
+            relations: ['user'] 
+        });
+    }
+
+    create(body: CreateOrdersDto): Promise<Order> {
+        this.orderRepository.create(body);
+        return this.orderRepository.save(body);
+    }
+
+    update(id: number, body: any) {
+        return this.orderRepository.update(id, body);
+    }
+
+    delete(id: number) {
+        return this.orderRepository.delete(id);
+    }
+}
