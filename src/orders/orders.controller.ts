0ed53bc6b72862fd003046f1018243a69d4afa68
Juan <nachobosque09@gmail.com>
2025-10-12 22:21:24 -0300

    CRUD de orders (lineas de drivers comentadas)

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrdersDto } from './entities/dto/create-orders.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(
+        private readonly ordersService: OrdersService
+    ) {}
+
+    @Get()
+    findAll() {
+        return this.ordersService.findAll();
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return this.ordersService.create(body);
+    }
+
+    @Put(':id')
+    update(@Param('id') id:string, @Body() body: CreateOrdersDto) {
+        return this.ordersService.update(+id, body);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string) {
+        return this.ordersService.delete(+id);
+    }
+
+}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/orders/orders';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order])],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
+  exports: [TypeOrmModule],
 })
 export class OrdersModule {}
