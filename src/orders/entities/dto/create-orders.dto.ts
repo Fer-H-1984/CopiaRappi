98aef21035ca2de0b22203ef424dc81783bd8a9a
Juan <nachobosque09@gmail.com>
2025-10-15 11:20:10 -0300

    fix: actualize controller, dtos y service de user y order y algunas cosas de vendors

--- src/orders/entities/dto/create-orders.dto.ts
+++ src/orders/entities/dto/create-orders.dto.ts
@@ -1,9 +1,17 @@
+import { IsDate, IsOptional, IsString } from "class-validator";
 import { User } from "src/users/entities/user/user";
 /* import { Driver } from "src/drivers/entities/drivers/drivers";
  */
 export class CreateOrdersDto {
+
     readonly User: User;
+
+    @IsDate()
     readonly createdAt: Date;    
+
 /*     readonly driver: Driver;
- */    readonly status?: string;
+ */    
+    @IsOptional()
+    @IsString()
+    readonly status?: string;
 }
