6d4818625f1a4e6d0c4976491a19acdb78ceb1c7
dgrsdgr <a>
2025-10-24 21:13:55 -0300

    auth_terminada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -16,19 +17,24 @@ import { VendorsController } from './vendors/vendors.controller';
 import { DriversService } from './drivers/drivers.service';
 import { BackofficeService } from './backoffice/backoffice.service';
 import { VendorsService } from './vendors/vendors.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ UsersModule, VendorsModule, DriversModule, OrdersModule, ProductsModule, BackofficeModule, TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',     
-  password: 'programacion4',         //cambiar contraseña si es necesario
-  database: 'copiaRappi',      
-  autoLoadEntities: true,
-  synchronize: true,    
-  logging: true,       
-}),],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // carga .env y lo hace global
+    AuthModule,
+    UsersModule, VendorsModule, DriversModule, OrdersModule, ProductsModule, BackofficeModule, TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',     
+      password: 'programacion4',         //cambiar contraseña si es necesario
+      database: 'copiaRappi',      
+      autoLoadEntities: true,
+      synchronize: true,    
+      logging: true,       
+    }),
+  ],
   controllers: [AppController, UserController, DriversController, BackofficeController, VendorsController],
   providers: [AppService, UsersService, DriversService, BackofficeService, VendorsService],
 })
