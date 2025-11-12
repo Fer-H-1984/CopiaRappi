7dfbf6de4e46029d70045036ff0fb896e9419de9
Juan <nachobosque09@gmail.com>
2025-11-12 17:43:32 -0300

    orderItem tiene una tabla intermedia

--- src/orders/entities/orders/orders.entity.ts
+++ src/orders/entities/orders/orders.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinTable } from 'typeorm';
 import { User } from '../../../users/entities/user/user.entity';
 import { Driver } from './../../../drivers/entities/drivers/driver.entity';
 import { Payment } from 'src/payments/payments/entities/payment.entity';
@@ -50,6 +50,7 @@ export class Order {
   driver: Driver; 
 
   @OneToMany(() => OrderItem, (orderItem) => orderItem.order, { cascade: true, eager: true })
+  @JoinTable({name: 'list_of_products', joinColumn: {name: 'orderId', referencedColumnName: 'id'}, inverseJoinColumn: {name: 'productId', referencedColumnName: 'id'}})
   items: OrderItem[];
 
   @ManyToOne(() => Vendor, (vendor) => vendor.order)
